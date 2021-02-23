664fb71449f6950d8a68f356a5e01580f85a08ef
Melissa <wolfaholic@users.noreply.github.com>
2021-02-23 01:36:10 -0800

    UserCard Complete
    Got UserCard Working and it is complete

--- Applied Javascript/GitHub_UserCard/GitHubCard/index.js
+++ Applied Javascript/GitHub_UserCard/GitHubCard/index.js
@@ -1,14 +1,18 @@
-/* Step 1: using axios, send a GET request to the following URL 
+import axios from 'axios';
+ 
+ /* Step 1: using axios, send a GET request to the following URL 
            (replacing the palceholder with your Github name):
            https://api.github.com/users/wolfaholic
 */
 axios.get("https://api.github.com/users/wolfaholic")
-    .then((response => {
-            console.log(response);
-        })
-        .catch(error => {
-            console.log("the data was not returned", error);
-        }),
+
+    .then(response => {
+          console.log(response);
+          })
+
+    .catch(error => {
+          console.log("the data was not returned", error);
+          })
 
 
 
@@ -62,21 +66,11 @@ axios.get("https://api.github.com/users/wolfaholic")
             following.textContent = `Following: ${profileInfo.following}`
             bio.textContent = `BIO: ${profileInfo.bio}`
 
-            return card
+            return card;
 
         }
 
-
-
-        /* Step 5: Now that you have your own card getting added to the DOM, either 
-                  follow this link in your browser https://api.github.com/users/Wolfaholic/followers 
-                  , manually find some other users' github handles, or use the list found 
-                  at the bottom of the page. Get at least 5 different Github usernames and add them as
-                  Individual strings to the friendsArray below.
-                  
-                  Using that array, iterate over it, requesting data for each user, creating a new card for each
-                  user, and adding that card to the DOM.
-        */
+        console.log(cardCreator);
 
         const followersArray = [
             'eaczechova',
@@ -95,30 +89,30 @@ axios.get("https://api.github.com/users/wolfaholic")
                 })
         })
 
-        /* Step 3: Create a function that accepts a single object as its only argument,
-                  Using DOM methods and properties, create a component that will return the following DOM element:
+        // /* Step 3: Create a function that accepts a single object as its only argument,
+        //           Using DOM methods and properties, create a component that will return the following DOM element:
 
-        <div class="card">
-          <img src={image url of user} />
-          <div class="card-info">
-            <h3 class="name">{users name}</h3>
-            <p class="username">{users user name}</p>
-            <p>Location: {users location}</p>
-            <p>Profile:  
-              <a href={address to users github page}>{address to users github page}</a>
-            </p>
-            <p>Followers: {users followers count}</p>
-            <p>Following: {users following count}</p>
-            <p>Bio: {users bio}</p>
-          </div>
-        </div>
+        // <div class="card">
+        //   <img src={image url of user} />
+        //   <div class="card-info">
+        //     <h3 class="name">{users name}</h3>
+        //     <p class="username">{users user name}</p>
+        //     <p>Location: {users location}</p>
+        //     <p>Profile:  
+        //       <a href={address to users github page}>{address to users github page}</a>
+        //     </p>
+        //     <p>Followers: {users followers count}</p>
+        //     <p>Following: {users following count}</p>
+        //     <p>Bio: {users bio}</p>
+        //   </div>
+        // </div>
 
-        */
+        // */
 
-        /* List of LS Instructors Github username's: 
-          tetondan
-          dustinmyers
-          justsml
-          luishrd
-          bigknell
-        */
+        // /* List of LS Instructors Github username's: 
+        //   tetondan
+        //   dustinmyers
+        //   justsml
+        //   luishrd
+        //   bigknell
+        // */
